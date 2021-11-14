bf118a698d231abd952134abcb0cc765e1639c50
kiwincardoza <rbkiwin1998@gmail.com>
2021-11-14 22:55:39 +0530

    Blogs and static files yet not working

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+
+def allblogs(request):
+	blogs = Blog.objects
+	return render(request, 'blogs/allblogs.html', {'blogs':blogs})
+
+
+def detail(request, blog_id):
+	blog_detail = get_object_or_404(Blog, pk=blog_id)
+	print (blog_detail)
+	return render(request, 'blogs/detail.html', {'blog_detail':blog_detail})
